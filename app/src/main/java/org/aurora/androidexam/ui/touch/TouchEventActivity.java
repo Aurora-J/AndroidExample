7445b53f22508effa6c152dd5a4661118c6cd393
aurora <code.zrj@gmail.com>
2016-07-07 15:39:22 +0800

    TouchEvent Test 界面

---
+++ app/src/main/java/org/aurora/androidexam/ui/touch/TouchEventActivity.java
@@ -0,0 +1,15 @@
+package org.aurora.androidexam.ui.touch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.aurora.androidexam.R;
+
+public class TouchEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_touch_event);
+    }
+}
